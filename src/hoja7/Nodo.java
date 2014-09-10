d2fc0bc1db5bfd967b8d00e9d91278d0082e1909
manuelgomez <manuelgomez@192.168.0.18>
2014-09-09 18:59:31 -0600

    hola

---
+++ src/hoja7/Nodo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hoja7;
+
+/**
+ *
+ * @author manuelgomez
+ */
+public class Nodo {
+    int peso;
+    char elemento ;
+    Nodo izquierda, derecha;
+    public Nodo(){
+    }
+    
+    public Nodo(char c, int p){
+        elemento = c;
+        peso = p;
+    }
+
+    
+}
